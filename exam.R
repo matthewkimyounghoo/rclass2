12343c9d7b3da5e55dba1c28be8ebd8bdd743333
matthewkimyounghoo <matthewkimyounghoo@gmail.com>
2018-07-14 18:20:14 +0900

    final commit

--- exam.R
+++ exam.R
@@ -7,4 +7,10 @@ df_exam
 mean(df_exam$english)
 mean(df_exam$science)
 
+#첫 행이 attribute 가 아닐 때 
+df_exam_novar <- read_excel("excel_exam_novar.xlsx",col_names=F)
+df_exam_novar
+
+mean(df_exam_novar$X__5)
+
 
